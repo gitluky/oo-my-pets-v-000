45a9f5592ba438305c191dd17889656d4621624a
Jonathan Luk <jluk87@gmail.com>
2018-03-21 21:26:03 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,17 +1,3 @@
 class Fish
-  attr_accessor :mood
-  attr_reader :name
-  def initialize(name)
-    @name = name
-    @mood = 'nervous'
-  end
-
-  def name
-    @name.dup.freeze
-  end
-
-  def buy_fish(fish)
-    self.pets[:fishs] << Fish.new(fish)
-  end
 
 end
